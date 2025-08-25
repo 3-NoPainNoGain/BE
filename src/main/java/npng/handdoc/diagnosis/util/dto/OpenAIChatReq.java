8ae4ed52f3992717e80ca5e47fa3c0a3fae2a10c
erika0915 <soohee2001@gmail.com>
2025-08-25 21:04:30 +0900

    feat: open ai 요청/응답 dto 작성 (#10)

--- src/main/java/npng/handdoc/diagnosis/util/dto/OpenAIChatReq.java
+++ src/main/java/npng/handdoc/diagnosis/util/dto/OpenAIChatReq.java
@@ -1,4 +1,18 @@
 package npng.handdoc.diagnosis.util.dto;
 
-public class OpenAIChatRequest {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class OpenAIChatReq {
+    @JsonProperty("model") private String model;
+    @JsonProperty("max_tokens") private Integer max_tokens;
+    @JsonProperty("temperature") private Double temperature;
+    @JsonProperty("top_p") private Double top_p;
+    @JsonProperty("messages") private List<Message> messages;
+    @JsonProperty("response_format") private Object responseFormat;
 }
