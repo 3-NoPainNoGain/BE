5da44ea0c263af946313a6ab20a4600af8f5cb5f
erika0915 <soohee2001@gmail.com>
2025-08-27 02:15:50 +0900

    feat: jpa에 baseentity 사용을 위해 @EnableJpaAuditing 추가 (#20)

--- src/main/java/npng/handdoc/HanddocApplication.java
+++ src/main/java/npng/handdoc/HanddocApplication.java
@@ -2,10 +2,12 @@ package npng.handdoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@EnableJpaAuditing
 public class HanddocApplication {
 
 	public static void main(String[] args) {
