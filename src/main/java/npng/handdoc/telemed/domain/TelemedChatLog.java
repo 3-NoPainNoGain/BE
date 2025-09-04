10b502cf48ddb0a7bfcad21e950388efd5ac7e18
erika0915 <soohee2001@gmail.com>
2025-09-04 22:37:11 +0900

    refactor: chatlog 엔티티 수정 (#35)

--- src/main/java/npng/handdoc/telemed/domain/TelemedChatLog.java
+++ src/main/java/npng/handdoc/telemed/domain/TelemedChatLog.java
@@ -1,21 +1,18 @@
 package npng.handdoc.telemed.domain;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import npng.handdoc.global.entity.BaseDocument;
 import npng.handdoc.telemed.domain.type.MessageType;
 import npng.handdoc.telemed.domain.type.Sender;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Document(collection = "telemed_chat_logs")
 public class TelemedChatLog extends BaseDocument {
 
@@ -24,11 +21,16 @@ public class TelemedChatLog extends BaseDocument {
 
     // roomId = Telemed.id (UUID) 문자열
     private String roomId;
+    List<Message> messageList;
 
-    private Sender sender;
-    private MessageType messageType;
-    private String message;
+    @Data
+    @Builder
+    public static class Message {
+        private Sender sender;
+        private MessageType messageType;
+        private String message;
 
-    @Builder.Default
-    private LocalDateTime timestamp = LocalDateTime.now();
+        @Builder.Default
+        private LocalDateTime timestamp = LocalDateTime.now();
+    }
 }
