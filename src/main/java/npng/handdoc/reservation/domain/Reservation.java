a89aed7ac8f3835454af9ca950d3ef204be6ead8
erika0915 <soohee2001@gmail.com>
2025-09-15 09:36:28 +0900

    feat: 예약에 대한 옵션을 추가하기 위해 엔티티 설정 추가 (#52)

--- src/main/java/npng/handdoc/reservation/domain/Reservation.java
+++ src/main/java/npng/handdoc/reservation/domain/Reservation.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import npng.handdoc.global.entity.BaseEntity;
+import npng.handdoc.reservation.domain.type.Option;
 import npng.handdoc.reservation.domain.type.ReservationStatus;
 import npng.handdoc.reservation.domain.type.Symptom;
 import npng.handdoc.user.domain.DoctorProfile;
@@ -11,6 +12,8 @@ import npng.handdoc.user.domain.User;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Entity
@@ -32,6 +35,13 @@ public class Reservation extends BaseEntity {
     @Column(name="status")
     @Enumerated(EnumType.STRING)
     private ReservationStatus status;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "reservation_interpretation",
+            joinColumns = @JoinColumn(name = "reservation_id"))
+    @Column(name="interpretation_option")
+    @Enumerated(EnumType.STRING)
+    private Set<Option> interpretationOption = new HashSet<>();
 
     @Column(name="description")
     private String description;
