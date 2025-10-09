f249a417ba1df553349894969d3f864fdd5b8829
erika0915 <soohee2001@gmail.com>
2025-10-09 17:50:51 +0900

    feat: cors 설정 추가 (#97)

--- src/main/java/npng/handdoc/global/config/SecurityConfig.java
+++ src/main/java/npng/handdoc/global/config/SecurityConfig.java
@@ -66,7 +66,8 @@ public class SecurityConfig {
                         "http://localhost:3000",
                         "https://handdoc.store",
                         "https://www.handdoc.store",
-                        "https://handdocfe.vercel.app")); // 추후에 추가
+                        "https://handdocfe.vercel.app",
+                        "https://ncp-contest19-fe.vercel.app")); // 추후에 추가
         configuration.setAllowedMethods(
                 Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(
