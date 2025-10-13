252f1c381d4cfdc1c04b362703d4c8e18f004373
erika0915 <soohee2001@gmail.com>
2025-10-13 23:55:28 +0900

    refactor: 시간 포맷 수정 (#66)

--- src/main/java/npng/handdoc/hospital/dto/response/HospitalHourResponse.java
+++ src/main/java/npng/handdoc/hospital/dto/response/HospitalHourResponse.java
@@ -3,17 +3,21 @@ package npng.handdoc.hospital.dto.response;
 import npng.handdoc.hospital.domain.HospitalHour;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public record HospitalHourResponse(
         String day,
-        LocalTime openTime,
-        LocalTime closeTime
+        String openTime,
+        String closeTime
 ) {
+
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
     public static HospitalHourResponse from(HospitalHour hospitalHour){
         return new HospitalHourResponse(
                 hospitalHour.getDay().name(),
-                hospitalHour.getOpenTime(),
-                hospitalHour.getCloseTime()
+                hospitalHour.getOpenTime().format(TIME_FORMATTER),
+                hospitalHour.getCloseTime().format(TIME_FORMATTER)
         );
     }
 }
