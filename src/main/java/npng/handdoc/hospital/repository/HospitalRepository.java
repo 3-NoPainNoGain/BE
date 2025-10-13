6c4699260881d71787f843a3c73343f0bfcd37db
erika0915 <soohee2001@gmail.com>
2025-10-13 20:07:43 +0900

    feat: repository 작성 (#66)

--- src/main/java/npng/handdoc/hospital/repository/HospitalRepository.java
+++ src/main/java/npng/handdoc/hospital/repository/HospitalRepository.java
@@ -1,4 +1,7 @@
 package npng.handdoc.hospital.repository;
 
-public interface HospitalRepository {
+import npng.handdoc.hospital.domain.Hospital;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HospitalRepository extends JpaRepository<Hospital, Long> {
 }
