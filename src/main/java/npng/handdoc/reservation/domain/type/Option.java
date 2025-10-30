dd926f9897e4df7042c1358387041d5b1804aa66
erika0915 <soohee2001@gmail.com>
2025-10-30 15:37:50 +0900

    feat: 예약 모드 옵션 추가 (#102)

--- src/main/java/npng/handdoc/reservation/domain/type/Option.java
+++ src/main/java/npng/handdoc/reservation/domain/type/Option.java
@@ -5,12 +5,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Option {
     SIGN_TO_TEXT("수어 텍스트 변환"),
-    VOICE_TO_TEXT("음성 텍스트 변환");
+    NORMAL_VOICE_TO_TEXT("음성 텍스트 변환"),
+    ABNORMAL_VOICE_TO_TEXT("구음 텍스트 변환");
 
     private final String label;
 
     public String getLabel() {
         return label;
     }
-    }
+}
 
